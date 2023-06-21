b65b0d55e91c0888dfe72cf9e1828beec88743fc
Roussel-030 <yvanroussel31@gmail.com>
2023-06-21 12:27:04 +0300

    Change color road optimal in yellow

--- withGraph/BelmanFord/config.js
+++ withGraph/BelmanFord/config.js
@@ -1,13 +1,13 @@
 var globalConfig = {    
     link : {
         defaultColor : "lightgrey", // default color of link "string"
-        choColor : "blue", // color of cho link "string", Chemin optimal
+        choColor : "yellow", // color of cho link "string", Chemin optimal
         labelColor : "white"
     },  
     node : {
         size : 40, // sizeof the node "integer"
         defaultColor : "lightgray", // color of the node "string"
-        choColor : "blue"// color of cho node "string"
+        choColor : "yellow"// color of cho node "string"
     },
     arrow: {
         fill: "white",
